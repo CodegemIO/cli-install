f6d77e45e394efe2f439e4e6c593ad24b50e972b
Kevin Shi <kevin.shi@shopify.com>
2021-07-10 07:18:17 -0400

    Install homebrew before anything else

--- install.sh
+++ install.sh
@@ -2,11 +2,19 @@
 
 __shellname="$(basename ${SHELL})"
 
+if ! command -v brew &> /dev/null
+then
+  echo "installing homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "homebrew is already installed"
+fi
+
 case "${__shellname}" in
   zsh)  __sh_config="${HOME}/.zshrc" ;;
   bash) __sh_config="${HOME}/.bashrc" ;;
   *)
-    >&2 \echo "Codegem CLI is incompatible with your shell (${__shellname})"
+    >&2 \echo "codegem cli is incompatible with your shell (${__shellname})"
     \exit 1
     ;;
 esac
@@ -14,13 +22,13 @@ esac
 __base_repository_path="${HOME}/src/github.com/CodegemIO"
 
 if [ -d "${__base_repository_path}/cli" ]; then
-  echo "Codegem CLI repository already cloned, skipping"
+  echo "codegem cli repository already cloned, skipping"
 else
-  echo "Downloading CLI repository"
+  echo "cloning cli repository"
   mkdir -p "${__base_repository_path}"
   cd "${__base_repository_path}" && git clone git@github.com:CodegemIO/cli.git
 
-  echo "Installing dependencies"
+  echo "installing cli dependencies"
   cd cli && bundle install
 fi
 
@@ -28,8 +36,8 @@ __required_line=". ~/src/github.com/CodegemIO/cli/cli.sh"
 
 if grep -Fxq "${__required_line}" "${__sh_config}"
 then
-  echo "Codegem CLI is already installed"
+  echo "codegem cli is already installed"
 else
   echo "${__required_line}" >> "${__sh_config}"
-  echo "Codegem CLI installed successfully, please restart your shell"
+  echo "codegem cli installed successfully, please restart your shell"
 fi
